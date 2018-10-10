c2325fdface79ab49d8d0d3b201168cabb51cf1b
junjungwook <wwwshe1@nate.com>
2018-10-10 13:22:05 +0900

    Initial Commit

---
+++ AppstoreDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppstoreDemo
+//
+//  Created by 서비스앱개발팀 on 2018. 10. 10..
+//  Copyright © 2018년 junjungwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AppstoreDemoTests/AppstoreDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppstoreDemoTests.swift
+//  AppstoreDemoTests
+//
+//  Created by 서비스앱개발팀 on 2018. 10. 10..
+//  Copyright © 2018년 junjungwook. All rights reserved.
+//
+
+import XCTest
+@testable import AppstoreDemo
+
+class AppstoreDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
